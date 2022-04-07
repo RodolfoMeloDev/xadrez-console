e06eac93d9172d300e975c3222b0a7ab98210dfc
Rodolfo Melo <rodolfo.melo.dev@gmail.com>
2022-04-06 21:00:22 -0300

    Implementada a jogada especial Promoção

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -154,6 +154,21 @@ namespace xadrez_console.xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = Tab.Peca(destino);
+
+            // #Jogada Especial Promoção
+            if (p is Peao)
+            {
+                if ((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preto && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -172,8 +187,7 @@ namespace xadrez_console.xadrez
                 Turno++;
                 MudarJogador();
             }
-
-            Peca p = Tab.Peca(destino);
+            
 
             // # Jogada Especial En Passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
